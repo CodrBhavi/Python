469aecc9e7af77e9b5b906f6abae1c017275e6b7
Bhavam Bokare <bhavambokare2008@outlook.com>
2021-12-10 13:07:13 +0530

    Asking user about his Favourite Superhero and print the name of the superhero

---
+++ superhero.py
@@ -0,0 +1,5 @@
+# We will ask user about his favourite superhero & we will print his favourite superhero name
+# Taking input from user
+super_hero = input("Who is your Favourite Superhero: ")
+# Printing superhero name
+print(super_hero)
