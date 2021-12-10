98bbb2deb373b702a8a95ac559391239cbfcabff
Bhavam Bokare <bhavambokare2008@outlook.com>
2021-12-10 13:04:08 +0530

    Taking input from user a complex number and then, print the real and imaginary part of the number

---
+++ com.py
@@ -0,0 +1,5 @@
+# Taking input from user
+a = eval(input("Enter a complex number: "))
+print(a.real)  # This line will print the real part of the complex number
+print(a.imag)  # This line will print the imaginary part of the complex number
+# End of the Program!!
