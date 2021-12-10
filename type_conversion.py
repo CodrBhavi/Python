02117518c08f078ea0d2bcd242c6f63e57523f9c
Bhavam Bokare <bhavambokare2008@outlook.com>
2021-12-10 13:09:05 +0530

    Asking user his/her recent age and then, + 1 and printing their current age

---
+++ type_conversion.py
@@ -0,0 +1,7 @@
+# We will ask user about their recent age & then, we will print their current age
+# Taking input from user
+recent_age = eval(input("Enter your recent age: "))
+# We are adding 1 with their current age
+current_age = int(recent_age) + 1
+# Printing their current age
+print("This is your Current age:",current_age)
