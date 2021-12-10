41f8a45c25b5d3e7a6411aa4b02a3b187bc8562f
Bhavam Bokare <bhavambokare2008@outlook.com>
2021-12-10 13:08:01 +0530

    Swapping Numbers Program in Python

---
+++ swap.py
@@ -0,0 +1,15 @@
+# Swapping Numbers Program
+# Taking input from user
+a = eval(input("Enter a Number for a: "))
+b = eval(input("Enter a Number for b: "))
+# Printing numbers when they are unswapped
+print("Before Swapping.......")
+print("a is ",a)
+print("b is ",b)
+# Swap Logic
+a,b = b,a
+# Printing numbers when they are swapped
+print("After Swapping........")
+print("a is ",a)
+print("b is ",b)
+# End of the Program!!
