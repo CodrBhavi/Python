e16102da7ed97804791fc7832fb19b8442fdb8bf
Bhavam Bokare <bhavambokare2008@outlook.com>
2021-12-11 18:35:07 +0530

    Bytes Data Type Program in Python

---
+++ bytes.py
@@ -0,0 +1,12 @@
+# Bytes Data Type Program in Python
+# Created a List in x variable
+x = [10,20,30,40,50]
+# Initialized x in bytes data type using b variable
+b = bytes(x)
+# Printing statements by slicing them!!
+print(b[0])
+print(b[-1])
+print(b[1])
+print(b[-2])
+
+
