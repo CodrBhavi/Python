9f33ae18a2513cc8d1aa1133854560b107e070af
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-01-06 15:21:21 +0530

    Python Program to Find the Maximum Value of number using Conditional Operator

---
+++ max.py
@@ -0,0 +1,5 @@
+# Python Program to Find the Maximum Value using Conditional Operator
+num1 = int(input("Enter a Number for num1: "))
+num2 = int(input("Enter a Number for num2: "))
+max=num1 if num1>num2 else num2
+print("Maximum Value: ",max)
