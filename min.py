5f151060330b1efc1f7b9a423fd594d61ad30ced
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-01-06 15:15:57 +0530

    Python Program to Find the Minimum Value of a Number using Conditional Operator

---
+++ min.py
@@ -0,0 +1,5 @@
+# Python Program to Find the Minimum Value using Conditional Operator
+num1 = int(input("Enter a Number for num1: "))
+num2 = int(input("Enter a Number for num2: "))
+min=num1 if num1<num2 else num2
+print("Minimum Value: ",min)
