101f43cc8fa2ef7768fca299db5980b5396211fa
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-01-07 15:01:19 +0530

    Python Program to check weather a password is correct or not

---
+++ pass.py
@@ -0,0 +1,5 @@
+# Python Program to check weather a password is correct or not
+# Taking Input from user
+pass_word = "Bhavam"
+password = input("Password: ")
+check = print("Password is correct!!"if password==pass_word else "Password is Incorrect!!")
