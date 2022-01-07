b35c64720fe0a1cdbfc40a947f18e42990bf1acd
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-01-07 14:49:58 +0530

    Python Program to Find out which number is bigger,smaller or equal

---
+++ ternary.py
@@ -0,0 +1,4 @@
+# Python Program to Find out which number is bigger,smaller or equal
+num1 = int(eval(input("Enter a Number for num1: ")))
+num2 = int(eval(input("Enter a Number for num2: ")))
+print("Both Numbers are equal" if num1==num2 else "num1 is less than num2" if num1<num2 else"num1 greater than num2 number")
