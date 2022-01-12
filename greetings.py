9b3ee223694d8ff276b986c2aec29edfe89f85b1
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-01-12 15:51:12 +0530

    # Python Program that will ask user his/her name and bot will wish him if his Bhavam

---
+++ greetings.py
@@ -0,0 +1,6 @@
+# Python Program that will ask user his/her name and bot will wish him if his Bhavam
+name = eval(input("Enter your Name: "))
+if name == "Bhavam" :
+			print("Happy Birthday Bhavam!!")
+else:
+	print("Have a Nice Day " + name) 
