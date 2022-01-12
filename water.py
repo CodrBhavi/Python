f8da48321f560a94073d5ba285a2905ec94109cb
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-01-12 14:58:54 +0530

    Drinking Water Remainder App

---
+++ water.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+			notification.notify(
+    		title = "Drink Water Now!!",
+    		message = "8 glasses of water a day, keeps a Doctor Away!!",
+    		app_icon = "C:\Python\icon.ico",
+    		timeout = 10
+			)
