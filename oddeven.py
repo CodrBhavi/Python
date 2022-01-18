e773dcb6ba24c84fdfa066cd4e44d3ae40e9efe9
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-01-18 14:54:25 +0530

    Python Program to check weather a number is odd or even

---
+++ oddeven.py
@@ -0,0 +1,9 @@
+# Python Program to check weather a number is odd or even
+# Taking an Integer Input
+num = int(input("Enter a Number: "))
+# If Entered Number is divisible with 2 then, the number is Odd Number
+if num%2==0:
+	print("This is Odd Number")
+# If Entered Number is not divisible with 2 then, the number is Even Number
+else:
+	print("This is Even Number")
