7f11909b40a20d871a9040d981152b105672777f
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-02-01 15:08:32 +0530

    Comparing Strings

---
+++ comparison.py
@@ -0,0 +1,8 @@
+s1 = input("Enter a String for s1: ")
+s2 = input("Enter a String for s2: ")
+if s1>s2:
+	print("s1 string is greater than s2 string")
+elif s1<s2:
+	print("s2 string is greater than s1 string")
+elif s1==s2:
+	print("s1 string is equal to s2 string")
