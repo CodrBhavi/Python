bd773aaef2e3e611af63a715252ee741709a141a
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-02-01 14:56:12 +0530

    Checking whether the sub-string is present in main string or not

---
+++ check.py
@@ -0,0 +1,7 @@
+# Checking whether the sub-string is present in main string or not
+main_str = input("Enter a main string: ") 
+sub_string = input("Enter a sub string: ")
+if sub_string in main_str:
+			print(sub_string,"is found in main string")
+else:
+	 print(sub_string,"is not found in main string")
