c2a4b6297c7cbfeb54919b06f2b5a06f9de60850
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-02-11 15:30:35 +0530

    Write a Function to take number input from user and print it's square

---
+++ square.py
@@ -0,0 +1,8 @@
+# Write a Function to take number input from user and print it's square
+def square(num):
+    print("The Square of the",num,"is",num*num)
+square(4)
+square(5)
+square(6)
+square(7)
+
