8e1199fd610e5f1c93df1b41a8f62ad458a95682
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-02-12 15:02:32 +0530

    Check whether a number is odd or even using Function

---
+++ foe.py
@@ -0,0 +1,13 @@
+# Check whether a number is odd or even using Function
+def fun(num):
+    if num%2==0:
+        print(num,"is an Even Number")
+    else:
+        print(num,"is Odd Number")
+fun(2)
+fun(3)
+fun(4)
+fun(5)
+fun(6)
+fun(7)
+
