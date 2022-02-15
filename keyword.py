95909703effec76da7f1ae9e2b430e758ba2853f
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-02-15 15:40:54 +0530

    Say Hello Bhavam Good Morning using keyword argumnets in function

---
+++ keyword.py
@@ -0,0 +1,5 @@
+# Say Hello Bhavam Good Morning using keyword argumnets in function
+def wish(name,msg):
+    print("Hello",name,msg)
+wish(name = "Bhavam",msg = "Good Morning!!")
+wish("Peter","Parker")
